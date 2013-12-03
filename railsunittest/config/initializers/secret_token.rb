d9ff00a3887f484dde958fe462740ff5089182e9
Tyler <josephtylerjones@gmail.com>
2013-12-03 10:19:14 -0500

    changed to postgres database

---
+++ railsunittest/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsunittest::Application.config.secret_key_base = 'ba4f291e7fca0d5a8133e6d04bd93c7f39824d451d18859d920785db825588105e18699ea0e1621a9f5e42683fe21f6ed884ae17cb9ff748ec08a0bdb2a2d7c7'
---
+++ railsunittest/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Railsunittest::Application.config.session_store :cookie_store, key: '_railsunittest_session'
